3b59a689a2f3844b851a4d1ff17c7140f07e1fb6
rodriggo <rodriggo.eet@gmail.com>
2024-05-12 23:07:39 -0300

    Algoritmo de busca binaria

---
+++ pesquisa_binaria.py
@@ -0,0 +1,18 @@
+def pesquisa_binaria(lista, item):    
+    numero_baixo = 0
+    numero_alto = len(lista) - 1
+
+    while numero_baixo <= numero_alto:
+        meio = (numero_baixo + numero_alto) // 2  
+        chute = lista[meio]  
+        if chute == item:
+            return meio
+        if chute > item:
+            numero_alto = meio - 1
+        else:
+            numero_baixo = meio + 1
+    return None
+
+minha_lista = [1, 3, 5, 7, 9]
+
+print(pesquisa_binaria(minha_lista, 7))
